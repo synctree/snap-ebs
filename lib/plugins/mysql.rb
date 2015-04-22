4bd9145e8540f3c956580a052dcf024bd5447d75
Bryan Conrad <bkconrad@gmail.com>
2015-04-22 16:32:55 -0700

    add some more mysql options

--- lib/plugins/mysql.rb
+++ lib/plugins/mysql.rb
@@ -1,5 +1,10 @@
 class EasyE::Plugin::Mysql < EasyE::Plugin
   def defined_options
-    { user: 'MySql Username' }
+    {
+      user: 'MySql Username',
+      pass: 'MySql Password',
+      port: 'MySql port',
+      host: 'MySql host'
+    }
   end
 end
