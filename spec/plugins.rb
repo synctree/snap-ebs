6c8d05f873d9bbc193f93fd08c7f4b446870b1b1
Bryan Conrad <bkconrad@gmail.com>
2015-04-21 22:04:48 -0700

    add better loading test

--- spec/plugins.rb
+++ spec/plugins.rb
@@ -3,8 +3,14 @@ describe EasyE::Plugin do
 
   context "on startup" do
     context "loaded plugins" do
+      before do
+        class TestClass < EasyE::Plugin
+        end
+      end
+
       subject { easy_e.plugins }
       it { is_expected.to be_an Array }
+      it { is_expected.to include TestClass }
     end
   end
 end
